355866c400490c5d4a5a5e3d86e6d35511aae4cc
betadayz <betadayz888@gmail.com>
2020-09-03 14:45:51 +0100

    Task number 10

---
+++ Task==10.py
@@ -0,0 +1,21 @@
+ASCII_SIZE = 256
+
+def getMaxOccuringChar(str):  
+	count = [0] * ASCII_SIZE 
+
+	max = -1
+	c = '' 
+ 
+	for i in str: 
+		count[ord(i)]+=1; 
+
+	for i in str: 
+		if max < count[ord(i)]: 
+			max = count[ord(i)] 
+			c = i 
+
+	return c 
+
+str = "sample string"
+print ("Max occurring character is " + getMaxOccuringChar(str)) 
+
